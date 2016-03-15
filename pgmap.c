8e6ab06a0426f4627ecab0a73525a82b38af4a06
Dmitry Semenikhin <semenikhindv@mail.ru>
2016-03-16 01:28:09 +0300

    fixes, now works

--- pgmap.c
+++ pgmap.c
@@ -1,6 +1,7 @@
 #include "postgres.h"
 #include "funcapi.h"
 
+#include "utils/lsyscache.h"
 #include "catalog/pg_type.h"
 #include "access/htup_details.h"
 #include "utils/syscache.h"
@@ -21,26 +22,30 @@ pgmap(PG_FUNCTION_ARGS)
 	Oid procId = PG_GETARG_OID(0);
 	AnyArrayType *array = (AnyArrayType *)PG_GETARG_ANY_ARRAY(1);
 
-	ArrayCoerceExprState *astate = (ArrayCoerceExprState *) palloc(sizeof(ArrayCoerceExprState));
-	FunctionCallInfoData *locfcinfo = (FunctionCallInfoData *) palloc(sizeof(FunctionCallInfoData));
+	ArrayMapState *amstate = (ArrayMapState *) palloc0(sizeof(ArrayMapState));
+	FunctionCallInfoData locfcinfo;
 
-	fmgr_info(procId, locfcinfo->flinfo);
+	FmgrInfo funcinfo;
+	fmgr_info(procId, &funcinfo);
 
-	tp = SearchSysCache1(TYPEOID, ObjectIdGetDatum(astate->resultelemtype));
+	InitFunctionCallInfoData(locfcinfo, &funcinfo, 3,
+							 InvalidOid, NULL, NULL);
+
+	tp = SearchSysCache1(TYPEOID, ObjectIdGetDatum(AARR_ELEMTYPE(array)));
 	if (HeapTupleIsValid(tp))
 	{
 		Form_pg_type typtup = (Form_pg_type) GETSTRUCT(tp);
-		locfcinfo->arg[1] = Int32GetDatum(PointerGetDatum(typtup));
+		locfcinfo.arg[1] = Int32GetDatum(PointerGetDatum(typtup->typtypmod));
 	}
 	ReleaseSysCache(tp);
 
-	locfcinfo->arg[0] = PointerGetDatum(array);
-//	locfcinfo.arg[1] = Int32GetDatum(typtup);
-	locfcinfo->argnull[0] = false;
-	locfcinfo->argnull[1] = false;
-	locfcinfo->argnull[2] = false;
+	locfcinfo.arg[0] = PointerGetDatum(array);
+	locfcinfo.arg[2] = BoolGetDatum(0);
+	locfcinfo.argnull[0] = false;
+	locfcinfo.argnull[1] = false;
+	locfcinfo.argnull[2] = false;
 
-	return array_map(locfcinfo, astate->resultelemtype, astate->amstate);
+	return array_map(&locfcinfo, get_func_rettype(procId), amstate);
 
 	PG_RETURN_ARRAYTYPE_P(array);
 }
